9862d9feab2652417150e0180c6dc2157304c715
Alejandro E <alejandro.escobedo.lozoya@gmail.com>
2020-10-09 16:05:06 -0700

    Commit Migration URP

---
+++ Assets/DontDestroyOnLoad.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestroyOnLoad : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+
+    // Update is called once per frame
+    void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
